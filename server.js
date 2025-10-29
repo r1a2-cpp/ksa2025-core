0495c7636fe1216426fc40e8df2d76767492ded4
riaz <riajul.swe@gmail.com>
2025-10-29 18:16:35 +0600

    database config

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from the backend!" });
+  res.json({ message: "Hello from the backend!", timestamp: new Date() });
 });
 
 // Start server
